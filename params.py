3c30a39b1191a4714ada0b4f04ff5098c94629d6
Greg Edelston <gredelston@gmail.com>
2019-06-18 19:39:00 -0600

    Redefine section lengths in terms of BEATS instead of Measures

--- params.py
+++ params.py
@@ -1,11 +1,11 @@
 import random
 
 # Bounds
-TEMPO_BOUNDS = (60, 128)
+TEMPO_BOUNDS = (60, 128) # Beats per minute
 NUM_SECTIONS_BOUNDS = (3, 8)
 
 # Lists
-SECTION_LENGTHS = [1.5, 2, 2.5, 3, 4, 6, 8]
+SECTION_LENGTHS = [6, 8, 10, 12, 16, 24, 32] # Beats
 
 def random_from_bounds(bounds, step=1):
     low, high = bounds
